b5bbc7cd343366e481f03117ee8b738c4217e075
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-01-19 17:56:15 +0100

    export constants to their own file

---
+++ constants.js
@@ -0,0 +1,43 @@
+const cells_generator_helper = ["A", "D", "F", "G", "V", "X"];
+const number_chars = {
+  A: 1,
+  B: 2,
+  C: 3,
+  D: 4,
+  E: 5,
+  F: 6,
+  G: 7,
+  H: 8,
+  I: 9,
+  J: 0,
+};
+const alphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+export { cells_generator_helper, number_chars, alphabet };
--- script.js
+++ script.js
@@ -1,44 +1,4 @@
-const cells_generator_helper = ["A", "D", "F", "G", "V", "X"];
-const number_chars = {
-  A: 1,
-  B: 2,
-  C: 3,
-  D: 4,
-  E: 5,
-  F: 6,
-  G: 7,
-  H: 8,
-  I: 9,
-  J: 0,
-};
-const alphabet = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
+import { cells_generator_helper, number_chars, alphabet } from "./constants.js";
 
 const adfgvxCipher = (text, key, board) => {
   let temp_text = "";
